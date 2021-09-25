dc74f5a09987e1822b000898015c155a6bf2eab7
lillipuu <stupidcats99@hotmail.com>
2021-09-25 11:35:23 +0300

    Space API and table integration

---
+++ src/space.js
@@ -0,0 +1,15 @@
+$(document).ready(async function() {
+  const response = await fetch('https://api.le-systeme-solaire.net/rest.php/bodies?filter[0]=isPlanet,cs,1&page=1,5&data=id,englishName,isPlanet,mass,gravity&order=gravity,asc')
+    .then(response => response.json());
+
+  for (let i = 0; i < response['bodies'].length; i++) {
+    const row = `
+      <tr>
+        <th class="row">${i + 1}</th>
+        <td>${ response['bodies'][i].englishName}</td>
+        <td>${ response['bodies'][i].gravity}</td>
+      </tr>`
+    ;
+    $('.js-planet-body').append(row);
+  }
+});
